b97c277a4492c437a31713950881c214610d04b5
Benedict-CS <benedicttiong.cs@gmail.com>
2024-05-22 22:38:54 +0800

    Initial commit

---
+++ typescript-version/full-version/src/@core/svg/Logo.tsx
@@ -0,0 +1,10 @@
+// React Imports
+import type { SVGAttributes } from 'react'
+
+const Logo = (props: SVGAttributes<SVGElement>) => {
+  return (
+    <img src="/images/illustrations/auth/logo.png" width={40}></img>
+  )
+}
+
+export default Logo
